f4ade11477a4061b1652e64728004cdc25331da9
David Lichtenberg <david.m.lichtenberg@gmail.com>
2017-01-14 18:37:30 -0500

    add model to flask app

--- app/app.py
+++ app/app.py
@@ -2,18 +2,45 @@ from flask import request
 from random import random
 from flask import Flask
 from flask_cors import CORS
+from flask import g
+from newspaper import Article
+
+from sklearn.externals import joblib
 
 app = Flask(__name__)
 CORS(app)
+
+text_clf = joblib.load('text_clf.pkl')
+
+# @app.before_first_request
+# def load_model():
+#     text_clf = joblib.load('text_clf.pkl')
+
 
 @app.route("/")
 def hello():
     return "hello world"
 
+
+def get_article_text(url):
+    article = Article(url, language='en')
+    article.download()
+    article.parse()
+    return article.text
+
+    # return {
+    #     "title": article.title,
+    #     "text": article.text
+    # }
+
+
 @app.route("/v1/bias/")
 def bias():
     #return str(random())
-    return str(request.args.get('url'))
+    url = request.args.get('url')
+    article_text = get_article_text(url)
+    score = text_clf.predict(article_text)
+    return str(score)
 
 if __name__ == "__main__":
     app.run()
