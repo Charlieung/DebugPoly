79247dced01326635904dd386fcfd3d11f2ed787
David Lichtenberg <david.m.lichtenberg@gmail.com>
2017-01-18 13:35:51 -0500

    remove unused code from app

--- app/app.py
+++ app/app.py
@@ -1,8 +1,6 @@
 from flask import request, jsonify
-from random import random
 from flask import Flask
 from flask_cors import CORS
-from flask import g
 from newspaper import Article
 
 from sklearn.externals import joblib
@@ -11,10 +9,6 @@ app = Flask(__name__)
 CORS(app)
 
 text_clf = joblib.load('app/text_clf.pkl')
-
-# @app.before_first_request
-# def load_model():
-#     text_clf = joblib.load('text_clf.pkl')
 
 
 @app.route("/")
@@ -28,18 +22,11 @@ def get_article(url):
     article.parse()
     return article
 
-    # return {
-    #     "title": article.title,
-    #     "text": article.text
-    # }
-
 
 @app.route("/v1/bias/")
 def bias():
-    #return str(random())
     url = request.args.get('url')
     article = get_article(url)
-    # print(article_text)
     score = text_clf.predict_proba([article.text])
     response = {
         "score": str(score[0][0]),
@@ -48,6 +35,7 @@ def bias():
         "date": article.publish_date.isoformat()[:10]
     }
     return jsonify(**response)
+
 
 if __name__ == "__main__":
     app.run()
